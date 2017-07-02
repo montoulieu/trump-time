1a25c48faf1dbb0a76a9bb247bac08953652de25
Pieter Montoulieu <pieter@Pieters-MacBook-Pro.local>
2017-07-02 17:07:50 -0600

    Responsive tweaks and build out of form and text replacement functionality

--- src/main.js
+++ src/main.js
@@ -9,6 +9,26 @@ $(document).ready(function(){
     fr.readAsDataURL(f);
   });
 
+  var trumpImages = [
+    'http://i.onionstatic.com/avclub/6364/23/16x9/960.jpg',
+    'https://s3.amazonaws.com/assets.forward.com/images/cropped/donaldtrump-0204-1454621037.jpg',
+  ];
 
+  $('button.btn-generate').click(function(e){
+
+    e.preventDefault();
+    Generate();
+
+  });
+
+  function Generate(){
+    $('input.form-control').each(function(index){
+      id   = $(this).attr('id');
+      text = $(this).val();
+      $('.time-text.' + id).text(text);
+      $(this).attr('value',text);
+    });
+
+  }
 
 });
